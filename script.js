9f2f7d6f036d8b749b681ab084d54e87fb811d85
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-02-01 05:02:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 function clock() {
-  const hours = document.querySelector('.hour');
-  const minutes = document.querySelector('.minutes');
-  const seconds = document.querySelector('.seconds');
-  const text = document.querySelector('.text');
-  const item = document.getElementById('item');
+  const hours = document.querySelector(".hour");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
+  const text = document.querySelector(".text");
+  const item = document.getElementById("item");
 
   const h = new Date().getHours();
   const m = new Date().getMinutes();
   const s = new Date().getSeconds();
 
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
 
-
-  hours.innerHTML = (h < 10) ? "0" + h : h;
-  minutes.innerHTML = (m < 10) ? "0" + m : m;
-  seconds.innerHTML = (s < 10) ? "0" + s : s;
-
-  const nama = item.value ? item.value : 'Kamu';
+  const nama = item.value ? item.value : "Bestie";
 
   if (h < 12) {
-    const ucap = `Selamat Pagi, <span>${nama}</span>`;
+    const ucap = `Pagi, <span>${nama}</span>`;
     text.innerHTML = ucap;
   }
   if (h >= 12 && h < 15) {
-    const ucap = `Selamat Siang, <span>${nama}</span>`;
+    const ucap = `Siang, <span>${nama}</span>`;
     text.innerHTML = ucap;
   }
   if (h >= 15 && h < 18) {
-    const ucap = `Selamat Sore, <span>${nama}</span>`;
+    const ucap = `Sore, <span>${nama}</span>`;
     text.innerHTML = ucap;
   }
   if (h >= 18 && h <= 24) {
-    const ucap = `Selamat Malam, <span>${nama}</span> <br/> Jangan lupa istirahat ya :)`;
+    const ucap = `Malam, <span>${nama}</span> <br/> Jangan lupa istirahat ya :)`;
     text.innerHTML = ucap;
   }
-
 }
 
 const interval = setInterval(clock, 1000);
